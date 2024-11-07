967621056f20f01c3bb0fd78b7c7251bd2934283
radwa-tester <radwakhodair@gmail.com>
2024-11-07 18:37:32 +0200

    end to end scenario

--- src/test/java/tests/testaddtask.java
+++ src/test/java/tests/testaddtask.java
@@ -2,9 +2,11 @@ package tests;
 
 
 import java.net.MalformedURLException;
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -25,17 +27,22 @@ public class testaddtask extends TestBase {
 		finishedtasks=new pages.finishedtaskspage(driver);
 		
 		Tasklistpage.clickaddtaskbtn();
-		Createtskpage.entertaskname("Study appium course1");
+		Createtskpage.entertaskname("Study appium coursev");
 		Createtskpage.savebtn();
 		//Dimension j=driver.findElement(By.xpath("//android.widget.TextView[contains(@resource-id, \"task_name\")]")).getSize();
 	//System.out.println(j);
-		Assert.assertEquals(driver.findElement(By.xpath("//android.widget.TextView[contains(@resource-id, \"task_name\")]")).getText(),"Study appium course1");
-		//Dimension i=driver.findElement(By.xpath("//android.widget.TextView[contains(@resource-id, \"task_name\")]")).getSize();
-		//driver.findElement(By.xpath("(//android.widget.CheckBox[contains(@resource-id, \"checkDone\")])[i]")).click();
-		//Assert.assertEquals(driver.findElement(By.xpath("//android.widget.TextView[contains(@resource-id, \"task_name\")]")).getText(),"Study appium course@@@");
+		Assert.assertEquals(driver.findElement(By.xpath("//android.widget.TextView[contains(@resource-id, \"task_name\")]")).getText(),"Study appium coursev");
+		
 		Tasklistpage.checkdone();
 		finishedtasks.showfinishedtasks();
-		Assert.assertEquals(driver.findElement(By.xpath("//android.widget.TextView[contains(@resource-id, \"task_name\")]")).getText(),"Study appium course1");
+		String textValue = "Study appium coursev"; // Define the text value here
+		String dynamicXPath = "//android.widget.TextView[@resource-id='com.splendapps.splendo:id/task_name' and @text='" + textValue + "']";
+		List<WebElement>elements = driver.findElements(By.xpath(dynamicXPath));
+		 for (WebElement element : elements) {
+	            String elementText = element.getText();
+	            Assert.assertEquals("Study appium coursev", textValue, elementText);
+	        }
+		//Assert.assertEquals(driver.findElement(By.xpath("//android.widget.TextView[contains(@resource-id, \"task_name\")]")).getText(),"Study appium coursev");
 		tearDown();
 	}
 
